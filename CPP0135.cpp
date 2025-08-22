542792fcfd79953facf394f5119c98c5c6e6366a
haikhongnghien <haibui1362006hd@gmail.com>
2025-08-22 11:09:18 +0700

    Create 	CPP0135.cpp

---
+++ CPP0135.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define FASTER cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+
+using namespace std;
+
+int check(int n) {
+    if(n < 2) return 0;
+    for(int i = 2 ; i <= sqrt(n) ; i++) {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+void solve(long long n) {
+
+    for(int i = 2 ; i <= sqrt(n) ; i++) {
+        if(check(i)) {
+            cout << i * i << " ";
+        }   
+    }
+    cout << endl;
+    
+}
+
+int main() {
+    
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        long long n;
+        cin >> n;
+        solve(n);
+    }
+
+    return 0;
+}
