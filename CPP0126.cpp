768a8fd2bb114e7793f8ee2a9714bf4b34e6e8b7
haikhongnghien <haibui1362006hd@gmail.com>
2025-08-22 11:06:04 +0700

    Create CPP0126.cpp

---
+++ CPP0126.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define FASTER cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+
+using namespace std;
+
+int check(int n) {
+    if(n < 2) return 0;
+    for(int i = 2 ; i <= sqrt(n) ; i++) {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+void solve(int a, int b) {
+
+    if(a > b) swap(a,b);
+    for(int i = a ; i <= b ; i++) {
+        if(check(i)) cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        int a,b;
+        cin >> a >> b;
+        solve(a,b);
+    }
+
+    return 0;
+}
