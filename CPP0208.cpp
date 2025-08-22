dd7beae09b15911fab60ac849cff34c7bc76fe73
haikhongnghien <haibui1362006hd@gmail.com>
2025-08-22 11:15:38 +0700

    Create 	CPP0208.cpp

---
+++ CPP0208.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define FASTER cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for(int i = 0 ; i < n ; i++) {
+        cin >> a[i];
+    }
+    sort(a,a + n);
+    cout << a[k - 1] << endl;
+}
+
+int main() {
+    
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
