4d83e937e0281b0f4877d2ab63e627eef78d4eda
haikhongnghien <haibui1362006hd@gmail.com>
2025-08-22 11:06:54 +0700

    Create 	CPP0127.cpp

---
+++ CPP0127.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define FASTER cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+
+using namespace std;
+
+int check(int n) {
+    if(n < 2) return 0;
+    for(int i = 2 ; i <= sqrt(n) ; i++) {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+void solve(int n) {
+
+    for(int i = 2 ; i <= n/2 ; i++) {
+        if(check(i) && check(n - i)) {
+            cout << i << " " << n - i << endl;
+            return;
+        }
+    }
+    cout << "-1" << endl;
+}
+
+int main() {
+    
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        solve(n);
+    }
+
+    return 0;
+}
